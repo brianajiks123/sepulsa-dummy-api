acddefb41b6786727c48246f908641cb3add28fa
brianajiks123 <brianajiks123@gmail.com>
2025-11-10 11:06:40 +0700

    Modify image type for operators

--- app/lib/data/articlesData.ts
+++ app/lib/data/articlesData.ts
@@ -4,14 +4,14 @@ export const articlesData = [
         title: 'Kuota Belajar Telkomsel: Ini Cara untuk Mendapatkannya!',
         description:
             'Telkomsel sebagai salah satu provider terbesar di Indonesia menghadirkan paket internet khusus pelajar...',
-        image: '/images/article1.jpg',
+        image: '/images/article1.webp',
         link: 'https://www.sepulsa.com/blog/kuota-belajar-telkomsel',
     },
     {
         id: 2,
         title: 'Berbagai Cara Pesan Tiket Kereta Api Secara Online',
         description: 'Tidak perlu lagi ke loket stasiun untuk membeli tiket kereta...',
-        image: '/images/article2.jpg',
+        image: '/images/article2.webp',
         link: 'https://www.sepulsa.com/blog/berbagai-cara-pesan-tiket-kereta-api-secara-online',
     },
     {
@@ -19,7 +19,7 @@ export const articlesData = [
         title: 'Cara Aktivasi KAI Access Hanya dengan 5 Langkah Saja!',
         description:
             'Kamu yang sering bepergian ke luar kota menggunakan kereta api, pasti sudah mengenai aplikasi resmi dari PT Kereta Api. KAI...',
-        image: '/images/article2.jpg',
+        image: '/images/article3.webp',
         link: 'https://www.sepulsa.com/blog/cara-aktivasi-kai-access-hanya-dengan-5-langkah-saja',
     },
 ];
